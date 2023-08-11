0aa4107d527ecab6c4a5b71231f56ddcd1cdd8ed
AashvikTyagi <93191910+AashvikTyagi@users.noreply.github.com>
2023-08-11 20:03:08 +0530

    Create SimpleVL6180X.ino
    Simple way to read from VL6180X.

---
+++ SimpleVL6180X.ino
@@ -0,0 +1,19 @@
+// libraries
+#include <Wire.h>
+#include "Adafruit_VL6180X.h"
+
+// declaring the sensor
+Adafruit_VL6180X tof = Adafruit_VL6180X();
+
+void setup() {
+  Serial.begin(9600); // serial monitor setup
+  while (!Serial) delay(1);
+  if (!tof.begin()) while (1); // sensor setup
+  Serial.print("Sensor Found!");
+}
+
+void loop() {
+  Serial.print("Dist (mm): ");
+  Serial.println(tof.readRange()); // prints the distance
+  delay(100); // wait 1/10th of a second
+}
